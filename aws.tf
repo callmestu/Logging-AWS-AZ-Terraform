148b709bcf0eaba7fc28e717598ea63af7502c1b
Chris Tomei <Chris.Tomei@SeisoLLC.com>
2021-10-14 09:26:25 -0400

    try this new s3 policy

--- aws.tf
+++ aws.tf
@@ -6,9 +6,10 @@ provider "aws" {
 
 #create required s3 bucket to store log data
 resource "aws_s3_bucket" "log_s3" {
-  bucket = "tf-cloudtrail-storage-test"
+  bucket = aws_s3_bucket.log_s3.id
 
-  policy = <<POLICY {
+  policy = <<POLICY 
+{
   "Id": "Policy1633988374100",
   "Version": "2012-10-17",
   "Statement": [
@@ -19,22 +20,23 @@ resource "aws_s3_bucket" "log_s3" {
         "s3:PutObject"
       ],
       "Effect": "Allow",
-      "Resource": "arn:aws:s3:::tf-cloudtrail-storage-test/prefix/AWSLogs",
+      "Resource": aws_s3bocket.log_s3.arn/tf-cloudtrail-storage-test/prefix/AWSLogs/*",
       "Principal": {
-        "AWS": [
-          "tf-cloudtrail-storage-test"
-        ]
+        "Service": "cloudtrail.amazonaws.com"
       }
     }
   ]
 }
 POLICY
 }
-#create cloudtrail resource
+
+#create cloudtrail resource - should have log file validation enabled
+#send cloudtrail logs to s3 for file storage, send to cloudwatch to facilitate real-time and historic activity logging 
 resource "aws_cloudtrail" "cloudtrail" {
   name                          = "TF_cloudtrail"
   s3_bucket_name                = aws_s3_bucket.log_s3.id
   include_global_service_events = true
+  #CloudWatchLogsLogGroupArn 
 }
 
 #create cloudwatch resource
@@ -50,10 +52,10 @@ resource "aws_cloudwatch_log_group" "cloudwatch_group" {
 #create guarduty resources
 
 
-#create aws config resources
+#create aws config resources/items
 
 
-#create aws x-ray resources
+#create aws x-ray resource - tracing recommended by aws security hub foundational best practices controls
 
 
 #create trusted advisor
@@ -62,7 +64,15 @@ resource "aws_cloudwatch_log_group" "cloudwatch_group" {
 
 #create aws inspector
 
+#aws detective
+
+#aws security hub
+
 #ci/cd monitoring
 
-
 #VPC flow logs - find a way to collect them
+
+#aws directory services logs
+
+#aws IoT device defender
+
