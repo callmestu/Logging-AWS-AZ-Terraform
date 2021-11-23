4225c74d105f9b88b17085fabc87374cc8b6ee7d
Chris Tomei <Chris.Tomei@SeisoLLC.com>
2021-11-23 09:25:11 -0500

    All resources represented in config

--- azure/main.tf
+++ azure/main.tf
@@ -31,3 +31,28 @@ resource "azurerm_log_analytics_workspace" "logAnalytics" {
   resource_group_name = azurerm_resource_group.azurePimAwsSSO.name
   sku                 = "PerGB2018"
 }
+resource "azurerm_automation_account" "automation_account" {
+  name                = "var.automation_account_name"
+  location            = azurerm_resource_group.PIM_VPC.location
+  resource_group_name = azurerm_resource_group.PIM_VPC.name
+
+  sku_name = "Basic"
+
+  tags = {
+    environment = "development"
+  }
+}
+resource "azurerm_automation_runbook" "var.runbook_name" {
+  name                    = "Get-AzureVMTutorial"
+  location                = azurerm_resource_group.azurePimAwsSSO.location
+  resource_group_name     = azurerm_resource_group.azurePimAwsSSO.name
+  automation_account_name = azurerm_automation_account.automation_account.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "Powershell sync script with secrets as variables"
+  runbook_type            = "PowerShellWorkflow"
+
+  publish_content_link {
+    uri = "https://raw.githubusercontent.com/callmestu/Logging-AWS-AZ-Terraform/main/azure/aad_sync.ps1"
+  }
+}
